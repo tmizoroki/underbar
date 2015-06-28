8a703c58679f73fc71bc014af2485b14cd028f19
Tomio Mizoroki <tmizoroki@gmail.com>
2015-06-27 22:32:14 -0700

    Complete ._uniq function

--- src/underbar.js
+++ src/underbar.js
@@ -99,6 +99,13 @@
 
   // Produce a duplicate-free version of the array.
   _.uniq = function(array) {
+    var unique = [];
+    _.each(array, function(item) {
+      if(_.indexOf(unique, item) === -1) {
+        unique.push(item);
+      }
+    });
+    return unique;
   };
 
 
